00dc37c90fac98ae06c0b58841c1dc99bd952ad0
Fábio Teixeira <fabioerubenteixeira@gmail.com>
2021-05-29 01:40:48 +0100

    Update Maze.h

--- Maze.h
+++ Maze.h
@@ -3,14 +3,21 @@
 #include "Post.h"
 class Maze {
 public:
+
 	Maze();
+
 	Maze(int nRows, int nCols);
-	void addPost(const Post& post);
+
+	void addPost(const Post& post); // add post to post vector
+
 	int getnumRows() const { return numRows; };
+
 	int getnumCols() const { return numCols; };
+
 	int postssize() const { return posts.size(); }
+
 	Post getpost(int indice) { return posts[indice]; }
-	// other methods
+
 private:
 
 	int numRows, numCols;
