6c28e894721470d4ca92ba90066ece72b1693806
Fábio Teixeira <fabioerubenteixeira@gmail.com>
2021-05-29 01:40:51 +0100

    Update Origem.cpp

--- Origem.cpp
+++ Origem.cpp
@@ -62,37 +62,39 @@ void maze_selection()
 }
 void menu()
 {
+	int reload;
 	unsigned short answer;
 	bool error = false;
-	cout << ">======>                >=>                   >=>          >=>       >=>                                " << endl;
-	cout << ">=>    >=>              >=>                   >=>          >> >=>   >>=>                                " << endl;
-	cout << ">=>    >=>      >=>     >=>         >=>     >=>>==>        >=> >=> > >=>    >=> >=>  >====>>=>   >==>   " << endl;
-	cout << ">> >==>       >=>  >=>  >=>>==>   >=>  >=>    >=>          >=>  >=>  >=>  >=>   >=>       >=>  >>   >=> " << endl;
-	cout << ">=>  >=>     >=>    >=> >=>  >=> >=>    >=>   >=>          >=>   >>  >=> >=>    >=>     >=>    >>===>>=>" << endl;
-	cout << ">=>    >=>    >=>  >=>  >=>  >=>  >=>  >=>    >=>          >=>       >=>  >=>   >=>    >=>     >>       " << endl;
-	cout << ">=>      >=>    >=>     >=>>==>     >=>        >=>         >=>       >=>   >==>>>==> >=======>  >====>  " << endl;
+	cout << endl << endl;
+	cout << "	>======>                >=>                   >=>          >=>       >=>                                " << endl;
+	cout <<	"	>=>    >=>              >=>                   >=>          >> >=>   >>=>                                " << endl;
+	cout <<	"	>=>    >=>      >=>     >=>         >=>     >=>>==>        >=> >=> > >=>    >=> >=>  >====>>=>   >==>   " << endl;
+	cout << "	>> >==>       >=>  >=>  >=>>==>   >=>  >=>    >=>          >=>  >=>  >=>  >=>   >=>       >=>  >>   >=> " << endl;
+	cout << "	>=>  >=>     >=>    >=> >=>  >=> >=>    >=>   >=>          >=>   >>  >=> >=>    >=>     >=>    >>===>>=>" << endl;
+	cout << "	>=>    >=>    >=>  >=>  >=>  >=>  >=>  >=>    >=>          >=>       >=>  >=>   >=>    >=>     >>       " << endl;
+	cout << "	>=>      >=>    >=>     >=>>==>     >=>        >=>         >=>       >=>   >==>>>==> >=======>  >====>  " << endl << endl << endl;
 	do
 	{
-		cout <<
-			"                                                                                                        " << endl <<
-			"                                           0~/~~<~~~~~|/~~~~<~~0                                        " << endl <<
-			"                                           |       _____       |                                        " << endl <<
-			"                                          .'______'     '______'.                                       " << endl <<
-			"                                         |  ____    > <    ____- |                                      " << endl <<
-			"                                        '   |   l         /   |   '                                     " << endl <<
-			"                                        |   |____l       /____|   |                                     " << endl <<
-			"                                        |                         |                                     " << endl <<
-			"                                        |       vvvvvvvvvvv       |                                     " << endl <<
-			"                                 __-__  |       ^^^^^^^^^^^       |  __-__                              " << endl <<
-			"--------------------------------|  |  |-----------------------------|  |  |-----------------------------" << endl <<
-			"|                               |  |  |                             |  |  |                            |" << endl <<
-			"|                                -- --            choose :           -- --                             |" << endl <<
-			"|                                               1 for Rules                                            |" << endl <<
-			"|                                                2 to Play                                             |" << endl <<
-			"|                                            3 for leaderboards                                        |" << endl <<
-			"|                                                0 to Exit                                             |" << endl <<
-			"|                                                                                                      |" << endl <<
-			"--------------------------------------------------------------------------------------------------------" << endl;
+		cout << 
+			"	                                                                                                      " << endl <<
+			"	                                           0~/~~<~~~~~|/~~~~<~~0                                        " << endl <<
+			"	                                           |       _____       |                                        " << endl <<
+			"	                                          .'______'     '______'.                                       " << endl <<
+			"	                                         |  ____    > <    ____- |                                      " << endl <<
+			"	                                        '   |   l         /   |   '                                     " << endl <<
+			"	                                        |   |____l       /____|   |                                     " << endl <<
+			"	                                        |                         |                                     " << endl <<
+			"	                                        |       vvvvvvvvvvv       |                                     " << endl <<
+			"	                                 __-__  |       ^^^^^^^^^^^       |  __-__                              " << endl <<
+			"	--------------------------------|  |  |-----------------------------|  |  |-----------------------------" << endl <<
+			"	|                               |  |  |                             |  |  |                            |" << endl <<
+			"	|                                -- --            choose :           -- --                             |" << endl <<
+			"	|                                               1 for Rules                                            |" << endl <<
+			"	|                                                2 to Play                                             |" << endl <<
+			"	|                                            3 for leaderboards                                        |" << endl <<
+			"	|                                                0 to Exit                                             |" << endl <<
+			"	|                                                                                                      |" << endl <<
+			"	--------------------------------------------------------------------------------------------------------" << endl;
 		cin >> answer;
 		if (cin.eof())
 		{
@@ -107,33 +109,19 @@ void menu()
 		}
 		else if (answer == 1)
 		{
-			cout << "--------------------------------------------------------------------------------------------------------" << endl <<
-				"|                                                RULES                                                 |" << endl <<
-				"--------------------------------------------------------------------------------------------------------" << endl <<
-				"|You will be placed in a maze made up of high-voltage fences.                                          |" << endl <<
-				"|There are robots that will try to destroy you.                                                        |" << endl <<
-				"|The only way you have to destroy them (and win the game)                                              |" << endl <<
-				"|is to make them crash with each other or make them crash                                              |" << endl <<
-				"|with the eletric fences (sorry, you can not punch them).                                              |" << endl <<
-				"|If you are the one hiting the robots or colliding with the fences you die.                            |" << endl <<
-				"|Every time you move the robots will also make a move going to your direction by the shortest path     |" << endl <<
-				"|(thats how you trick them). The robots have no vision sensors but                                     |" << endl <<
-				"|they have an accurate odour sensor that allows them to follow the player!                             |" << endl <<
-				"|                                                                                                      |" << endl <<
-				"|                                                                                                      |" << endl <<
-				"|you can only move to one of the 8 neighbour cells of your current cell.                               |" << endl <<
-				"|these are the letters you can use to move :                                                           |" << endl <<
-				"|                                     Q           W            E                                       |" << endl <<
-				"|                                                                                                      |" << endl <<
-				"|                                     A          you           D                                       |" << endl <<
-				"|                                                                                                      |" << endl <<
-				"|                                     Z           X            C                                       |" << endl <<
-				"|                                                                                                      |" << endl <<
-				"|you can also stay in the same place by choosing 'S'.                                                  |" << endl <<
-				"--------------------------------------------------------------------------------------------------------" << endl;
-			int reload;
+			string rule;
+			ifstream rule_screen("RULES.TXT");
+
+			while (getline(rule_screen, rule))
+			{
+				cout << rule << endl;  //display leaderboard
+			}
+
+
 			cout << endl << "type 0 to return to menu" << endl;
+
 			cin >> reload;
+
 			while (!cin.good() || reload != 0)
 			{
 				cout << "this is not a valid choice, please try again." << endl;
@@ -141,6 +129,7 @@ void menu()
 				cin.ignore(INT_MAX, '\n');
 				cin >> reload;
 			}
+			
 			error = false;
 			menu();
 		}
@@ -148,6 +137,20 @@ void menu()
 		{
 			error = false;
 			maze_selection();
+
+			cout << endl << "type 0 to return to menu" << endl;
+
+			cin >> reload;
+
+			while (!cin.good() || reload != 0)
+			{
+				cout << "this is not a valid choice, please try again." << endl;
+				cin.clear();
+				cin.ignore(INT_MAX, '\n');
+				cin >> reload;
+			}
+
+			menu();
 			return;
 		}
 		else if (answer == 3)
@@ -155,6 +158,19 @@ void menu()
 			error = false;
 			Leaderboard MenuLeaderboard;
 			MenuLeaderboard.OpenInMenu();
+
+			cout << endl << "type 0 to return to menu" << endl;
+			cin >> reload;
+
+			while (!cin.good() || reload != 0)
+			{
+				cout << "this is not a valid choice, please try again." << endl;
+				cin.clear();
+				cin.ignore(INT_MAX, '\n');
+				cin >> reload;
+			}
+
+			error = false;
 			menu();
 		}
 		else if (answer == 0)
@@ -172,11 +188,4 @@ void menu()
 int main()
 {
 	menu();
-	//int num;
-	//Time score;
-	//score.TimeStart();
-	//cout << "input";
-	//cin >> num;
-	//score.TimeEnd();
-	//score.Score();
 }
